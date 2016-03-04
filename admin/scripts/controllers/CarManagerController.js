b6afd813888755a8181a1122b2fc5672a5c54dea
junsworth <jonathan@bubbleworks.co.za>
2016-03-04 21:51:48 +0200

    Adding Car, Customer and Supplier Manager Controllers

---
+++ admin/scripts/controllers/CarManagerController.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name familyCarsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the familyCarsApp
+ */
+angular.module('familyCarsApp')
+  .controller('CarManagerCtrl', function ($scope, $rootScope, request, $location, filterFilter) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.makeSelect = -1;
+    $scope.modelSelect = -1;
+
+    $scope.models = [];
+
+    makes();
+    models();
+
+    function makes() {
+	    
+	    request.get('/cars/makes').then(function(makes) {
+	      $scope.makes = makes;
+	    });
+
+  	}
+
+  	function models() {
+	    
+	    request.get('/cars/models').then(function(models) {
+	      $scope.models = models;
+	    });
+
+  	}
+
+  	$scope.filterModels = function(id) {
+  		$scope.filteredModels = filterFilter($scope.models, {'MakeId':id});
+  	}
+
+  });
