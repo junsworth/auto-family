029dbb7e8e2ad703d4ce7d1c2c12e5fc996677cb
junsworth <jonathan@bubbleworks.co.za>
2016-03-06 09:59:02 +0200

    Added service requests for Supplier Manager Controller

--- admin/scripts/controllers/SupplierManagerController.js
+++ admin/scripts/controllers/SupplierManagerController.js
@@ -8,10 +8,76 @@
  * Controller of the familyCarsApp
  */
 angular.module('familyCarsApp')
-  .controller('SupplierManagerCtrl', function () {
+  .controller('SupplierManagerCtrl', function ($scope, $rootScope, $location, request) {
+    
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.name = "";
+    $scope.address = "";
+    $scope.addresstwo = "";
+    $scope.city = "";
+    $scope.email = "";
+    $scope.phone = "";
+
+    suppliers();
+
+    $scope.add = function() {
+
+	    request.post('/suppliers/add', {
+	      name: $scope.name,
+	      address: $scope.address,
+	      addresstwo: $scope.addresstwo,
+	      city: $scope.city,
+	      email: $scope.email,
+	      phone: $scope.phone
+	    }).then(function(supplier) {
+	      console.log('supplier ' + JSON.stringify(supplier));
+	      $location.path('#/suppliers');
+	    });
+
+  	};
+
+  	function suppliers() {
+	    request.get('/suppliers/suppliers').then(function(suppliers) {
+	      $scope.suppliers = suppliers;
+
+	      $scope.viewby = 4;
+
+	      $scope.totalItems = $scope.suppliers.length;
+
+	      $scope.currentPage = 1;
+
+	      $scope.itemsPerPage = $scope.viewby;
+
+	      $scope.maxSize = 5; //Number of pager buttons to show
+
+	    });
+	}
+
+	$scope.setPage = function (pageNo) {
+
+	  $scope.currentPage = pageNo;
+
+	};
+
+
+	$scope.pageChanged = function() {
+
+	  console.log('Page changed to: ' + $scope.currentPage);
+
+	};
+
+
+	$scope.setItemsPerPage = function(num) {
+
+	  $scope.itemsPerPage = num;
+
+	  $scope.currentPage = 1; //reset to first paghe
+
+	}
+
   });
