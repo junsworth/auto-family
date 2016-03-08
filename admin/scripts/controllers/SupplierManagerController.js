ff99f7a499d3867e8a1e5c89e3f45f7f96a1bccb
junsworth <jonathan@bubbleworks.co.za>
2016-03-08 20:43:01 +0200

    Included edit and delete functionality for SupplierManager

--- admin/scripts/controllers/SupplierManagerController.js
+++ admin/scripts/controllers/SupplierManagerController.js
@@ -2,31 +2,37 @@
 
 /**
  * @ngdoc function
- * @name familyCarsApp.controller:MainCtrl
+ * @name familyCarsApp.controller:SupplierManagerCtrl
  * @description
- * # MainCtrl
+ * # SupplierManagerCtrl
  * Controller of the familyCarsApp
  */
 angular.module('familyCarsApp')
-  .controller('SupplierManagerCtrl', function ($scope, $rootScope, $location, request) {
+  .controller('SupplierManagerCtrl', function ($scope, $rootScope, $location, request, $routeParams) {
     
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    if($routeParams.id) {
+		request.get('/suppliers/get/:id', {
+			id: $routeParams.id
+		}).then(function(supplier) {
+			$scope.name = supplier.name;
+		    $scope.address = supplier.address;
+		    $scope.addresstwo = supplier.addresstwo;
+		    $scope.city = supplier.city;
+		    $scope.email = supplier.email;
+		    $scope.phone = supplier.phone;
+		});
+    } else {
+    	$scope.name = "";
+	    $scope.address = "";
+	    $scope.addresstwo = "";
+	    $scope.city = "";
+	    $scope.email = "";
+	    $scope.phone = "";
 
-    $scope.name = "";
-    $scope.address = "";
-    $scope.addresstwo = "";
-    $scope.city = "";
-    $scope.email = "";
-    $scope.phone = "";
-
-    suppliers();
+	    suppliers();
+    }
 
     $scope.add = function() {
-
 	    request.post('/suppliers/add', {
 	      name: $scope.name,
 	      address: $scope.address,
@@ -36,10 +42,19 @@ angular.module('familyCarsApp')
 	      phone: $scope.phone
 	    }).then(function(supplier) {
 	      console.log('supplier ' + JSON.stringify(supplier));
-	      $location.path('#/suppliers');
+	      $location.path('/suppliers');
 	    });
-
   	};
+
+  	$scope.edit = function(supplier) {
+  		$location.path('/addsupplier').search({id: supplier.id});
+  	};
+
+  	$scope.delete = function(supplier) {
+      request.delete('/suppliers/delete/:id', {
+        id: supplier.id
+      }).then(suppliers).then($location.path('/suppliers'));
+    };
 
   	function suppliers() {
 	    request.get('/suppliers/suppliers').then(function(suppliers) {
@@ -59,25 +74,16 @@ angular.module('familyCarsApp')
 	}
 
 	$scope.setPage = function (pageNo) {
-
 	  $scope.currentPage = pageNo;
-
 	};
-
 
 	$scope.pageChanged = function() {
-
 	  console.log('Page changed to: ' + $scope.currentPage);
-
 	};
 
-
 	$scope.setItemsPerPage = function(num) {
-
 	  $scope.itemsPerPage = num;
-
 	  $scope.currentPage = 1; //reset to first paghe
-
-	}
+	};
 
   });
