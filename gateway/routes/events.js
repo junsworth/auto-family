4575e74dbbbc8ad5f1c4474783c50a8419097e3e
junsworth <jonathan@bubbleworks.co.za>
2016-03-31 17:56:39 +0200

    Adding gateway services for events.

---
+++ gateway/routes/events.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var db = require('../models/database');
+var lodash = require('lodash');
+
+exports.get = function(req, res) {
+  db.Event.findById(parseInt(req.params.id, 10))
+    .then(function(result) {
+      res.send(result);
+    });
+};
+
+exports.events = function(req, res) {
+  db.Event.findAll({
+    id: ['id']
+  }).then(function(results) {
+    res.send(lodash.map(results, function(element, index, list) {
+      return lodash.pick(element.toJSON(), ['id', 'title', 'header','description', 'images', 'startDate', 'endDate']);
+    }));
+  });
+};
+
+exports.add = function(req, res, next) {
+  db.Event.create({
+    title: req.body.title,
+    header: req.body.header,
+    description: req.body.description,
+    images: req.body.images,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate
+  }).catch(function(error) {
+    next('ERROR_ADDING_SERVICE');
+  }).then(function(offer) {        
+    if (offer) {
+      res.json(offer);
+    } else {
+      next('INVALID_OFFER_REGISTRATION');          
+    }
+  }).then(function() {
+    //res.redirect('/');
+  });
+};
+
+exports.update = function(req, res) {
+  db.Event.findById(req.params.id)
+    .then(function(result) {
+      return result.updateAttributes(req.body);
+    }).then(function() {
+      res.status(204).send();
+    });
+};
+
+exports.delete = function(req, res, next) {
+  db.sequelize.transaction().then(function(t) {
+    db.Event.find(req.params.id, {
+      transaction: t
+    }).then(function(result) {
+      return result.destroy({
+        transaction: t
+      });
+    }).then(function() {
+      return t.commit();
+    }).then(function() {
+      res.status(204).send();
+    }, function(err) {
+      t.rollback();
+      next(err);
+    });
+  });
+};
