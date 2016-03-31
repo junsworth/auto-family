9748ab9b05818d720c1c63e33e670fce622cf5fd
junsworth <jonathan@bubbleworks.co.za>
2016-03-31 17:54:59 +0200

    Adding supplier service for front-end and refactoring SupplierController's service calls to here.

---
+++ scripts/services/suppliers.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('familyCarsApp').
+factory('SupplierService', function(request){
+
+	var service = {
+
+		create: function(name, address, addresstwo, city, email, phone){
+			var promise = request.post('/suppliers/add', {
+				name: name,
+				address: address,
+				addresstwo: addresstwo,
+				city: city,
+				email: email,
+				phone: phone
+		    }).then(function(supplier) {
+		    	return supplier;
+		    });
+		    return promise;
+		},
+		supplier: function(id) {
+			var promise = request.get('/suppliers/get/:id', {
+				id: id
+			}).then(function(supplier) {
+	      		return supplier;
+	    	});
+	    	return promise;
+		},
+		suppliers: function() {
+			var promise = request.get('/suppliers/suppliers').then(function(suppliers) {
+	      		return suppliers;
+	    	});
+	    	return promise;
+		},
+		update: function(supplier) {
+			var promise = request.put('/suppliers/update/:id', supplier, {
+	        	id: supplier.id
+	      	});
+			return promise;
+		},
+		delete: function(id) {
+			var promise = request.delete('/suppliers/delete/:id', {
+		        id: id
+		    });
+		    return promise;
+		}
+
+	};
+
+	return service;
+
+});
