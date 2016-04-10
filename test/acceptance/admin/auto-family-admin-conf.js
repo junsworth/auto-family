3947eae068c31a2260b4100693e60c10b7fbade3
junsworth <jonathan@bubbleworks.co.za>
2016-04-10 10:42:44 +0200

    Deleting files

--- test/acceptance/admin/auto-family-admin-conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['admin-user-add-supplier-spec.js'],
-  capabilities: {
-    browserName: 'firefox'
-  }
-}
--- test/mock/User.js
+++
@@ -1,6 +0,0 @@
-function User(id, email, password, type) {
-	this.id = id;
-	this.email = email;
-	this.password = password;
-	this.UserTypeId = type;
-}
